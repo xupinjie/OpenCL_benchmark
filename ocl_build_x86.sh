eb0569b69285d5d86910ce03ca22970e14060072
xupinjie <xupinjie321@163.com>
2024-09-03 15:30:50 +0800

    [fix] toolchain error

--- ocl_build_x86.sh
+++ ocl_build_x86.sh
@@ -2,6 +2,6 @@ processor_num=`cat /proc/cpuinfo | grep processor | grep -v grep | wc -l`
 export PPL_BUILD_THREAD_NUM=$processor_num
 echo "processor_num is $processor_num"
 
-sh ./build.sh -DCMAKE_TOOLCHAIN_FILE=$ANDROID_NDK/build/cmake/android.toolchain.cmake \
+sh ./build.sh \
 -DCL_TARGET_OPENCL_VERSION=220 \
 -DPPLNN_USE_OCL=ON
